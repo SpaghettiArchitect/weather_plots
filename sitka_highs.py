41727457414ae4908e8e264c5d2917ea5f33c959
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-22 22:17:50 -0600

    Update dataset used in plot
    - The plot now uses the Sitka dataset that contains a whole year worth
    of data.

--- sitka_highs.py
+++ sitka_highs.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def main() -> None:
-    path = Path(r"data\sitka_weather_07-2021_simple.csv")
+    path = Path(r"data\sitka_weather_2021_simple.csv")
     lines = path.read_text().splitlines()
 
     reader = csv.reader(lines)
@@ -26,7 +26,7 @@ def main() -> None:
     ax.plot(dates, highs, color="red")
 
     # Format plot
-    ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+    ax.set_title("Daily High Temperatures, 2021", fontsize=24)
     ax.set_xlabel("", fontsize=16)
     fig.autofmt_xdate()
     ax.set_ylabel("Temperature (F)", fontsize=16)
